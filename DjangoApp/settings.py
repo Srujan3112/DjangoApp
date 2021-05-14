36e6b21cf5b6be9319aae86d0da3f3baf9c050eb
Srujan3112 <srujanpenta@gmail.com>
2021-05-14 22:34:49 +0530

    Added required files

--- DjangoApp/settings.py
+++ DjangoApp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -130,3 +130,4 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'Base/static'),
     os.path.join(BASE_DIR, 'Cart/static'),
 ]
+django_heroku.settings(locals())
