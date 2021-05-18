a4aa988497f1159f871aa3ca3f27a1b92d33bb5f
Srujan3112 <srujanpenta@gmail.com>
2021-05-18 10:44:13 +0530

    fixed search box error

--- Base/views/productlist.py
+++ Base/views/productlist.py
@@ -17,7 +17,10 @@ class ProductList(LoginRequiredMixin, ListView):
         context['count'] = context['products'].count()
         search_input = self.request.GET.get('search-area') or ''
         if search_input:
-            context['products'] = context['products'].filter(title__startswith=search_input)
+            products=Product.objects.all()
+            products=products.filter(title__startswith=search_input)
+            context['products']=products
+            # context['products'] = context['products'].filter(title__startswith=search_input)
         context['search_input'] = search_input
         cart, _ = Cart.objects.get_or_create(user=self.request.user)
         context['quantity'] = cart.quantity
